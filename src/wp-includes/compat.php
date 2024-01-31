182ed203398bfec1c8967666dc52de5e124ceb02
Aaron Jorbin <jorbin@git.wordpress.org>
2024-01-31 18:30:30 +0000

    WP-r57455: General: Backport polyfills for `str_ends_with()` and `str_starts_with()`.
    Merges https://core.trac.wordpress.org/changeset/52040, https://core.trac.wordpress.org/changeset/56016, and https://core.trac.wordpress.org/changeset/56015 to 4.9 branch.
    
    WP:Props ocean90, SergeyBiryukov, desrosj, joemcgill, jorbin, mukesh27.
    
    Conflicts:
    - src/wp-includes/compat.php
    
    ---
    
    Merges https://core.trac.wordpress.org/changeset/57455 / WordPress/wordpress-develop@9f317ee541 to ClassicPress.

--- src/wp-includes/compat.php
+++ src/wp-includes/compat.php
@@ -502,6 +502,7 @@ if ( ! function_exists( 'is_iterable' ) ) {
 	}
 }
 
+<<<<<<< HEAD
 if ( ! function_exists( 'array_key_first' ) ) {
 	/**
 	 * Polyfill for array_key_first() function added in PHP 7.3.
@@ -570,4 +571,50 @@ if ( ! defined( 'IMAGETYPE_WEBP' ) ) {
 // IMG_WEBP constant is only defined in PHP 7.0.10 or later.
 if ( ! defined( 'IMG_WEBP' ) ) {
 	define( 'IMG_WEBP', IMAGETYPE_WEBP ); // phpcs:ignore PHPCompatibility.Constants.NewConstants.imagetype_webpFound
+=======
+if ( ! function_exists( 'str_starts_with' ) ) {
+	/**
+	 * Polyfill for `str_starts_with()` function added in PHP 8.0.
+	 *
+	 * Performs a case-sensitive check indicating if
+	 * the haystack begins with needle.
+	 *
+	 * @since 5.9.0
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle   The substring to search for in the `$haystack`.
+	 * @return bool True if `$haystack` starts with `$needle`, otherwise false.
+	 */
+	function str_starts_with( $haystack, $needle ) {
+		if ( '' === $needle ) {
+			return true;
+		}
+
+		return 0 === strpos( $haystack, $needle );
+	}
+}
+
+if ( ! function_exists( 'str_ends_with' ) ) {
+	/**
+	 * Polyfill for `str_ends_with()` function added in PHP 8.0.
+	 *
+	 * Performs a case-sensitive check indicating if
+	 * the haystack ends with needle.
+	 *
+	 * @since 5.9.0
+	 *
+	 * @param string $haystack The string to search in.
+	 * @param string $needle   The substring to search for in the `$haystack`.
+	 * @return bool True if `$haystack` ends with `$needle`, otherwise false.
+	 */
+	function str_ends_with( $haystack, $needle ) {
+		if ( '' === $haystack ) {
+			return '' === $needle;
+		}
+
+		$len = strlen( $needle );
+
+		return substr( $haystack, -$len, $len ) === $needle;
+	}
+>>>>>>> 9f317ee541 (General: Backport polyfills for `str_ends_with()` and `str_starts_with()`.)
 }

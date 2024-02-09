f89b043446d5c998471de13a521ef25f7fce9fc7
xxsimoxx <simone@gieffeedizioni.it>
2024-02-09 17:09:59 +0100

    Fix WP version and notes

--- src/wp-admin/about.php
+++ src/wp-admin/about.php
@@ -157,6 +157,22 @@ require ABSPATH . 'wp-admin/admin-header.php';
 				printf(
 					/* translators: %s: WordPress version number */
 					__( '<strong>Version %s</strong> addressed some security issues.' ),
+					'4.9.25'
+				);
+				?>
+				<?php
+				printf(
+					/* translators: %s: HelpHub URL */
+					__( 'For more information, see <a href="%s">the blog post</a>.' ),
+					esc_url( __( 'https://wordpress.org/news/2024/01/wordpress-6-4-3-maintenance-and-security-release/' ) ),
+				);
+				?>
+			</p>
+			<p>
+				<?php
+				printf(
+					/* translators: %s: WordPress version number */
+					__( '<strong>Version %s</strong> addressed some security issues.' ),
 					'4.9.24'
 				);
 				?>
